2f9efb59d4ee59986bfea50781cc1e9475a06483
Victor <vic.g8@protonmail.com>
2022-01-09 20:16:21 +0100

    Constructeur ?

--- Scrabble.java
+++ Scrabble.java
@@ -1,6 +1,20 @@
 import java.util.*;
 public class Scrabble {
     private static int []nbPointsJeton = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 10, 1, 2, 1, 1, 3, 8, 1, 1, 1, 1, 4, 10, 10, 10, 10};
+    private Joueur[] joueurs;
+    private int numJoueur; // joueur courant (entre 0 et joueurs.length-1)
+    private Plateau plateau;
+    private MEE sac;
+
+
+    public Scrabble () {
+        this.sac
+    }
+    public static void main (String[]args) {
+        
+    }
+
+
 
     public static String saisirChaine () {
         try (Scanner clavier = new Scanner(System.in)) {
